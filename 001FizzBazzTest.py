7a021752393234e57595808dcf9fee50d9e8f725
ALLAN KAKAI <47603211+ALLANKAKAI@users.noreply.github.com>
2019-06-22 21:28:02 +0300

    Update 001FizzBazzTest.py

--- 001FizzBazzTest.py
+++ 001FizzBazzTest.py
@@ -1,9 +1,9 @@
-for x in range(1, 101):
-    if x % 3 == 0 and x % 5 == 0:
-        print("%d is FizzBazz" % x)
-    if x % 3 == 0:
-        print("%d is Fiz" % x)
-    elif x % 5 == 0:
-        print("%d is Bazz" % x)
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("%d is FizzBazz" % number)
+    if number % 3 == 0:
+        print("%d is Fiz" % number)
+    elif number % 5 == 0:
+        print("%d is Bazz" % number)
 
 
